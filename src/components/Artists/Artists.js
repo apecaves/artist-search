20823a17309b551a2b84b8f19339f5077514c1bc
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-21 15:25:20 -0700

    created Artists comp

---
+++ src/components/Artists/Artists.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Artist from './Artist';
+
+function Artists({ artistList }) {
+  const artistItem = artistList.map(artist => {
+    return (
+      <li key={artist.id}>
+        <Artist artist={artist}/>
+      </li>
+    );
+  });
+
+  return <ul>{artistItem}</ul>;
+}
+
+Artists.propTypes = {
+  artistList: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default Artists;
