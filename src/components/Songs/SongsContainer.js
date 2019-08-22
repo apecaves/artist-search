ccfe7c0a413fc43cf9cb65234c9613deb43f656c
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-22 14:17:58 -0700

    created SongsContainer comp

---
+++ src/components/Songs/SongsContainer.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Songs from './Songs';
+import { getSongList } from '../../services/musicBrainsApi';
+
+export default class SongsContainer extends Component {
+  static propTypes = {
+    match: PropTypes.object.isRequired
+  };
+
+  state = {
+    songs: []
+  };
+
+  componentDidMount = () => {
+    return getSongList(this.props.match.params.id)
+      .then(({ songs }) => {
+        this.setState({ songs });
+      });
+  };
+
+  render() {
+    const { songs } = this.state;
+
+    return <Songs songList={songs} />;
+  }
+}
