a7e7e821727cac5e422e62b019be819e747170e2
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-22 14:44:07 -0700

    brought in a new Route to App for song

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import SearchPage from './SearchPage/SearchPage';
+import HomePage from './HomePage/HomePage';
 import ReleasesContainer from './Releases/ReleasesContainer';
 import SongsContainer from './Songs/SongsContainer';
+import LyricContainer from './Lyrics/LyricContainer';
 
 export default function App() {
   return (
     <Router>
-      <Route exact path="/" component={SearchPage} />
+      <Route exact path="/" component={HomePage} />
       <Route path="/artist/:id" component={ReleasesContainer} />
-      <Route path="/release/:id" component={SongsContainer}/>
+      <Route path="/release/:id" component={SongsContainer} />
+      <Route path="/song/:title" component={LyricContainer} />
     </Router>
   );
 }
