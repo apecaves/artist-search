2348ccc98078abaca0a7848614b7f30ff68eb559
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-22 15:18:28 -0700

    brought artistName into our Routes to be able to store the artist name to use across the pages

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,15 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './HomePage/HomePage';
 import ReleasesContainer from './Releases/ReleasesContainer';
 import SongsContainer from './Songs/SongsContainer';
-import LyricContainer from './Lyrics/LyricContainer';
+import LyricsContainer from './Lyrics/LyricsContainer';
 
 export default function App() {
   return (
     <Router>
       <Route exact path="/" component={HomePage} />
-      <Route path="/artist/:id" component={ReleasesContainer} />
-      <Route path="/release/:id" component={SongsContainer} />
-      <Route path="/song/:title" component={LyricContainer} />
+      <Route path="/artist/:artistName/:id" component={ReleasesContainer} />
+      <Route path="/release/:artistName/:id" component={SongsContainer} />
+      <Route path="/song/:artistName/:title" component={LyricsContainer} />
     </Router>
   );
 }
