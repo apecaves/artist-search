21fa2c3d7c3018d1713686826e1807898a593407
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-22 15:21:01 -0700

    brought artistName prop into the Release Link URL

--- src/components/Releases/Release.js
+++ src/components/Releases/Release.js
@@ -3,17 +3,18 @@ import PropTypes from 'prop-types';
 import placeholder from '../../../assets/cover-art-placeholder.jpg';
 import { Link } from 'react-router-dom';
 
-function Release({ release }) {
-  const imgURL = release['cover-art-archive'].front ? `http://coverartarchive.org/release/${release.id}/front` : placeholder;
+function Release({ release, artistName }) {
+  const imgURL = release['cover-art-archive'].front
+    ? `http://coverartarchive.org/release/${release.id}/front`
+    : placeholder;
   return (
     <>
-      <Link to={`/release/${release.id}`}>{release.title}</Link>
+      <Link to={`/release/${artistName}/${release.id}`}>{release.title}</Link>
       <p>{release.date}</p>
       <p>{release.country}</p>
-      <Link to={`/release/${release.id}`}>
+      <Link to={`/release/${artistName}/${release.id}`}>
         <img src={imgURL} />
       </Link>
-  
     </>
   );
 }
@@ -25,7 +26,8 @@ Release.propTypes = {
     date: PropTypes.string.isRequired,
     country: PropTypes.string,
     'cover-art-archive': PropTypes.object.isRequired
-  }).isRequired
+  }).isRequired,
+  artistName: PropTypes.string.isRequired
 };
 
 export default Release;
